358f96cbd35c5cf53a48fff978908d5628b8a647
BenjaminJedar <benjisaintpaul@hotmail.fr>
2022-10-29 17:54:49 +0200

    mise en place du dossier images dans le gitignore pour éviter de charger les images sur le repo

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
+const path = require('path');   
+const cors = require('cors');   
 const userRoutes = require('./routes/user');
-
+const sauceRoutes = require('./routes/sauce');
+ 
 
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 mongoose.connect('mongodb+srv://Ben:Clyde@cluster0.gne33ba.mongodb.net/?retryWrites=true&w=majority',
     {
@@ -21,13 +25,18 @@ mongoose.connect('mongodb+srv://Ben:Clyde@cluster0.gne33ba.mongodb.net/?retryWri
 
 
 
+/* //Middleware permettant d'empêcher les erreurs de CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
     next();
-});
+}); */
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
+ 
 
 module.exports = app;   
